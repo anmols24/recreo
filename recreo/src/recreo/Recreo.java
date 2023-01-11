285a02c4ddd7d5d12ee015014e6239f7c6c8c20d
Anmol singh <anmolsinghbatti24@gmail.com>
2023-01-11 11:01:47 +0100

    1 commit

---
+++ recreo/src/recreo/Recreo.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package recreo;
+
+/**
+ *
+ * @author anmol
+ */
+public class Recreo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        hola
+                kjklj
+                lkjkj7
+                        lkl
+    }
+    
+}
